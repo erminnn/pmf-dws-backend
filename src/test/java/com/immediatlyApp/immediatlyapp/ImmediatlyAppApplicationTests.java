362baed40579fbc6d7d2f19658ac98362ed680a4
Ermin Omeragic <erminnn@hotmail.com>
2020-07-11 00:11:40 +0200

    updated user model (added user image)

---
+++ src/test/java/com/immediatlyApp/immediatlyapp/ImmediatlyAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.immediatlyApp.immediatlyapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImmediatlyAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
